81f4e57c6c10617c80bef697df205917193cde9e
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-16 15:47:37 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,13 +1,26 @@
-<?php
-session_start();
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
-    if ($username == 'admin' && $password == '123') {
-        $_SESSION['user'] = $username;
-        header("location:mainpage.php");
-    } else {
-        echo "incorrect username and password";
-        require "login.html";
-    }
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <form action="login.php" method="POST">
+        <input type="text" name="username" value="">
+        <input type="password" name="password" value="">
+        <input type="submit" name="login">
+    </form>
+    <?php
+	session_start();
+	$username = $_POST['username'] ?? '';
+	$password = $_POST['password'] ?? '';
+    	if ($username == 'admin' && $password == '123') {
+        	$_SESSION['user'] = $username;
+        	header("location:mainpage.php");
+    	} else {
+        	echo "incorrect username and password";
+        	require "login.html";
+    	}
  
-?>
+    ?>
+</body>
+</html>
+ 
