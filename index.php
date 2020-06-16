2e46c3a6d17cf3d5ccd6bd09b342168429f27744
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-16 16:13:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,15 @@
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-    <form action="login.php" method="POST">
-        <input type="text" name="username" value="">
-        <input type="password" name="password" value="">
-        <input type="submit" name="login">
-    </form>
-    <?php
-	session_start();
-	$username = $_POST['username'] ?? '';
-	$password = $_POST['password'] ?? '';
-    	if ($username == 'admin' && $password == '123456') {
-        	$_SESSION['user'] = $username;
-        	header("location:mainpage.php");
-    	} else {
-        	echo "incorrect username and password";
-        	require "login.html";
-    	}
- 
-    ?>
-</body>
-</html>
- 
+<?php
+$conn_string = "host=ec2-3-216-129-140.compute-1.amazonaws.com port=5432 dbname=d96skcv75vgijc user=ymhxtmwgaprvqp password=2e80c41436598527d8a60a5d19d0937b0e5d36b0f0d8ffe467220ded8c167a15"
+$dbconn = pg_connect($conn_string);
+if(isset($_POST['username'])){
+	$username = $_POST['username'];
+}
+if(isset($_POST['pass'])){
+	$pass = $_POST['pass'];
+}
+$result = pg_query($dbconn, query : "select * from tbl_account");
+while ($row = pg_fetch_row($result)){
+	if($row[0]==$username && $row[1]==$pass){
+		echo" Login success! ";
+	}
+}
