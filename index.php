80582fbbc940fcbbb7a6c323b4653b00da765413
gitkimtran <66985888+gitkimtran@users.noreply.github.com>
2020-06-18 08:21:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="Connect.php" method="POST" accept-charset="utf-8">
+User: <input type="text" name="username" value="" placeholder="username">
+Password: <input type="password" name="pass" value="" placeholder="*********">
+<input type="submit" name="" value="Login">
+</form>
+</body>
+</html>
